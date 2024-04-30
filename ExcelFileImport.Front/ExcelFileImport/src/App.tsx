162fbcb2c89e32242b706f3c3d606e552531873a
Vinícius Rodel <vinicius_rodel@hotmail.com>
2024-04-30 17:29:46 -0300

    feat: Criado o projeto de frontend em react.
    Criada a primeira tela de importaçao de arquivo com as funções:
      - Seleção do arquivo a ser enviado;
      - Conversão para base 64;
      - Envio a API de importação de arquivo;

---
+++ ExcelFileImport.Front/ExcelFileImport/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExcelImport from '../ExcelImport/ExcelImport';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Excel Importer</h1>
+            <ExcelImport />
+        </div>
+    );
+};
+
+export default App;
